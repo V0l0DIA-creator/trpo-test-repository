062a412b1bb5ba58288cccc26b84894b74b25c0d
Drimer <drimer1321@gmail.com>
2021-06-13 16:09:44 +0300

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, введи 1 или 2");
+            int iNum = int.Parse(Console.ReadLine());
+            if(iNum == 1)
+            {
+                Console.WriteLine("Ты ввел число 1");
+            }
+            else
+            {
+                //помощь
+            }
+        }
+    }
+}
